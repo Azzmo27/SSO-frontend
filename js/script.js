11f0ec67c579a30432ddc42f1d0e43c8e431358e
Azra <Azmo0001@stud.kea.dk>
2024-12-02 21:23:08 +0100

    frontend design

---
+++ js/script.js
@@ -0,0 +1,19 @@
+document.getElementById('btn-become-member').addEventListener('click', function() {
+    window.location.href = "becomeMember.html"; // Redirect to the "Bliv Medlem" page
+});
+
+document.getElementById('btn-view-events').addEventListener('click', function() {
+    window.location.href = "events.html"; // Redirect to the events page
+});
+
+document.getElementById('btn-cph').addEventListener('click', function() {
+    window.location.href = "copenhagen.html"; // Redirect to Copenhagen department page
+});
+
+document.getElementById('btn-odense').addEventListener('click', function() {
+    window.location.href = "odense.html"; // Redirect to Odense department page
+});
+
+document.getElementById('btn-aarhus').addEventListener('click', function() {
+    window.location.href = "aarhus.html"; // Redirect to Aarhus department page
+});
